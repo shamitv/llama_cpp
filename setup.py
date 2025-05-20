212726ea3599ac41b41d41e0f42ae0221bd26a42
shamitv <github@shamit.in>
2025-05-20 23:22:16 +0530

    Include vendor directory in source distribution

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='llama_cpp',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         # Add any dependencies here
     ],
