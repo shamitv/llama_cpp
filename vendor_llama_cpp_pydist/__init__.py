10f1d1372e14c32f170304d871a505d80bdaef3b
shamitv <github@shamit.in>
2025-05-25 17:15:34 +0530

    Update submodule path and enhance __init__.py documentation

--- vendor_llama_cpp_pydist/__init__.py
+++ vendor_llama_cpp_pydist/__init__.py
@@ -1,1 +1,3 @@
-# This file makes 'vendor' a Python package
+# This file makes 'vendor_llama_cpp_pydist' a Python package
+# This is to ensure that python scripts are available when package is installed. 
+# For example : https://github.com/ggml-org/llama.cpp/blob/master/convert_hf_to_gguf.py
