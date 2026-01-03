763aafb93298e3b5ce9c3baf88a0280d8d4a5446
shamitv <github@shamit.in>
2026-01-03 23:32:37 +0530

    Bump version to 0.20.0 and update subproject commit for llama.cpp

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def _read_text(path):
 
 setup(
     name='llama_cpp_pydist',
-    version='0.19.0',
+    version='0.20.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
